7983045e890e6e8600d39234c8953c6650d86165
Bryan Webster <BWebster@qs1.com>
2017-09-11 16:24:56 -0400

    First Commit

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+    apiKey: "AIzaSyBQtVkQE2q_vnflSUwJ6u8L0VJ5gc9KV1M",
+    authDomain: "fir-crud-89c8c.firebaseapp.com",
+    databaseURL: "https://fir-crud-89c8c.firebaseio.com",
+    projectId: "fir-crud-89c8c",
+    storageBucket: "fir-crud-89c8c.appspot.com",
+    messagingSenderId: "105251077764"
+  };
